fef36e51fd7ee1ebbe99cdc84750744591f4b9c4
xiaodino <ruby@taiko.xyz>
2025-05-29 01:05:15 -0700

    Fix: floor voting power display by truncating decimals

--- packages/ui/plugins/delegates/components/DelegateListItem.tsx
+++ packages/ui/plugins/delegates/components/DelegateListItem.tsx
@@ -86,7 +86,7 @@ export const DelegateListItem: React.FC<IDelegateListItemProps> = (props) => {
           <div className="flex h-12 flex-col gap-y-2">
             <p className="text-sm md:text-base">
               <span className="text-neutral-500">Voting Power: </span>
-              {formatEther(votingPower || BigInt(0))}
+              {formatEther(votingPower || 0n).split(".")[0]}
             </p>
           </div>
         </If>
