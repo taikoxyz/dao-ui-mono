907fbfe7756e2ba2114586e4a9bf9e7907c7b1a8
bearni95 <bernatcanal@gmail.com>
2025-06-03 12:19:47 +0200

    unified box display

--- packages/ui/components/proposal/cardResources.tsx
+++ packages/ui/components/proposal/cardResources.tsx
@@ -1,29 +1,179 @@
 import getSecurityCouncilMemberData from "@/utils/getSecurityCouncilMemberData";
 import { IGqlProposalMixin } from "@/utils/gql/getGqProposal";
 import type { IProposalResource } from "@/utils/types";
+import { gql } from "@apollo/client";
 import { Card, CardEmptyState, Heading, IconType, Link } from "@aragon/ods";
 import React from "react";
-import { Address } from "viem";
+import { Address, zeroAddress } from "viem";
+import SecurityCouncilProfiles from "../../security-council-profiles.json";
+
+import { OptimisticProposal } from "@/plugins/optimistic-proposals/utils/types";
+import { useProposalStatus } from "@/plugins/optimistic-proposals/hooks/useProposalVariantStatus";
+import { Else, ElseIf, If, Then } from "@/components/if";
+import { getShortTimeDiffFrom } from "@/utils/dates";
+import { HeaderSection } from "@/components/layout/header-section";
+import { getTagVariantFromStatus } from "@/utils/ui-variants";
+import { capitalizeFirstLetter } from "@/utils/text";
+import { Publisher } from "@/components/publisher";
+import { isAddressEqual } from "viem";
+import { useEncryptionAccounts as useEncryptionAccountsEmergency } from "@/plugins/security-council/hooks/useEncryptionAccounts";
 
 interface ICardResourcesProps {
   displayLink?: boolean;
   resources?: IProposalResource[];
   title: string;
   gqlProposal?: IGqlProposalMixin;
+  relatedProposal?: IGqlProposalMixin;
 }
 
 export const CardResources: React.FC<ICardResourcesProps> = (props) => {
   return (
     <div className="flex w-full flex-col gap-y-4">
       <DefaultDAOInfoCard {...props} />
-      <CreationCard {...props} />
-      <ApprovalsCard {...props} />
-      <VetoCard {...props} />
-      <ExecutionCard {...props} />
+      <TransactionsCard {...props} />
     </div>
   );
 };
 
+const TransactionsCard: React.FC<ICardResourcesProps> = (props) => {
+  if (!props.gqlProposal || !props.gqlProposal.creationTxHash || !props.relatedProposal) {
+    return null;
+  }
+  const { gqlProposal, relatedProposal } = props;
+
+  const creator = relatedProposal?.creator || gqlProposal?.creator || zeroAddress;
+
+  const executor = relatedProposal?.executor || gqlProposal?.executor;
+
+  const { data: encryptionAccounts } = useEncryptionAccountsEmergency();
+  const owner =
+    encryptionAccounts?.find(
+      ({ appointedAgent }) => appointedAgent && creator && isAddressEqual(appointedAgent, creator)
+    )?.owner || undefined;
+  const executioner =
+    encryptionAccounts?.find(
+      ({ appointedAgent }) => appointedAgent && executor?.address && isAddressEqual(appointedAgent, executor.address)
+    )?.owner || undefined;
+  const creatorProfile = owner && SecurityCouncilProfiles.find((p: any) => isAddressEqual(p.address, owner));
+  const executorProfile =
+    executioner && SecurityCouncilProfiles.find((p: any) => isAddressEqual(p.address, executioner));
+
+  const approvals = relatedProposal?.approvers || gqlProposal?.approvers || [];
+  const vetoes = gqlProposal?.vetoes || relatedProposal?.vetoes || [];
+  return (
+    <Card className="flex flex-col gap-y-4 p-6 shadow-neutral">
+      <Heading size="h4">Transactions</Heading>
+      <table>
+        <tbody>
+          <tr>
+            <td colSpan={2}>Creation</td>
+          </tr>
+          <tr>
+            <td>
+              <Link target="_blank" href={`https://etherscan.io/address/${relatedProposal.creator}`} variant="primary">
+                {creatorProfile?.name}
+              </Link>
+            </td>
+            <td>
+              <Link
+                target="_blank"
+                href={`https://etherscan.io/tx/${relatedProposal.creationTxHash}`}
+                variant="primary"
+                iconRight={IconType.LINK_EXTERNAL}
+              >
+                tx
+              </Link>
+            </td>
+          </tr>
+          <tr>
+            <td colSpan={2}>Approvals</td>
+          </tr>
+          {approvals.map((approver, i) => (
+            <tr>
+              <td>
+                <Link
+                  target="_blank"
+                  href={`https://etherscan.io/address/${approver.address}`}
+                  variant="primary"
+                  key={i}
+                >
+                  {getSecurityCouncilMemberData(approver.address).name || approver.address}
+                </Link>
+              </td>
+              <td>
+                <Link
+                  target="_blank"
+                  href={`https://etherscan.io/tx/${approver.txHash}`}
+                  variant="primary"
+                  iconRight={IconType.LINK_EXTERNAL}
+                  key={i}
+                >
+                  tx
+                </Link>
+              </td>
+            </tr>
+          ))}
+          {/* vetoes */}
+
+          {vetoes.length > 0 && (
+            <tr>
+              <td colSpan={2}>Vetoes</td>
+            </tr>
+          )}
+
+          {vetoes.map((vetoer, i) => (
+            <tr>
+              {" "}
+              <td>
+                <Link target="_blank" href={`https://etherscan.io/address/${vetoer.address}`} variant="primary" key={i}>
+                  {vetoer.address.replace(/^(.{6}).*(.{4})$/, "$1...$2")}
+                </Link>
+              </td>
+              <td>
+                <Link
+                  target="_blank"
+                  href={`https://etherscan.io/tx/${vetoer.txHash}`}
+                  variant="primary"
+                  iconRight={IconType.LINK_EXTERNAL}
+                  key={i}
+                >
+                  tx
+                </Link>
+              </td>
+            </tr>
+          ))}
+
+          {/* execution */}
+          {!vetoes.length && (
+            <tr>
+              <td colSpan={2}>Execution</td>
+            </tr>
+          )}
+
+          {!vetoes.length && (
+            <tr>
+              <td>
+                <Link target="_blank" href={`https://etherscan.io/address/${executor?.address}`} variant="primary">
+                  {executorProfile?.name}
+                </Link>
+              </td>
+              <td>
+                <Link
+                  target="_blank"
+                  href={`https://etherscan.io/tx/${executor?.txHash}`}
+                  variant="primary"
+                  iconRight={IconType.LINK_EXTERNAL}
+                >
+                  tx
+                </Link>
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </Card>
+  );
+};
 const CreationCard: React.FC<ICardResourcesProps> = (props) => {
   if (!props.gqlProposal || !props.gqlProposal.creationTxHash) {
     return null;
