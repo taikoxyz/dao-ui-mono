d49f243b9e49dfa30e294692f31242ac4a55c8a9
bearni95 <bernatcanal@gmail.com>
2025-06-16 21:26:07 +0200

    merge main changes

--- packages/ui/src/components/proposal/cardResources.tsx
+++ packages/ui/src/components/proposal/cardResources.tsx
@@ -8,7 +8,6 @@ import { isAddressEqual } from "viem";
 import { useEncryptionAccounts as useEncryptionAccountsEmergency } from "@/plugins/security-council/hooks/useEncryptionAccounts";
 import { IGqlProposalMixin } from "@/utils/gql/types";
 
-
 interface ICardResourcesProps {
   displayLink?: boolean;
   resources?: IProposalResource[];
@@ -27,37 +26,37 @@ export const CardResources: React.FC<ICardResourcesProps> = (props) => {
 };
 
 const TransactionsCard: React.FC<ICardResourcesProps> = (props) => {
-  if (!props.gqlProposal || !props.gqlProposal.creationTxHash || !props.relatedProposal) {
-    return <Card className="flex flex-col gap-y-4 p-6 shadow-neutral">Loading tx info...</Card>;
-  }
   const { gqlProposal, relatedProposal } = props;
 
-  const creator = relatedProposal?.creator || gqlProposal?.creator || zeroAddress;
+  const creator = relatedProposal?.creator ?? gqlProposal?.creator ?? zeroAddress;
 
-  const executor = relatedProposal?.executor || gqlProposal?.executor;
+  const executor = relatedProposal?.executor ?? gqlProposal?.executor;
 
   const { data: encryptionAccounts } = useEncryptionAccountsEmergency();
   const owner =
     encryptionAccounts?.find(
       ({ appointedAgent }) => appointedAgent && creator && isAddressEqual(appointedAgent, creator)
-    )?.owner || undefined;
+    )?.owner ?? undefined;
   const executioner =
     encryptionAccounts?.find(
       ({ appointedAgent }) => appointedAgent && executor?.address && isAddressEqual(appointedAgent, executor.address)
-    )?.owner || undefined;
+    )?.owner ?? undefined;
   const creatorProfile = owner && SecurityCouncilProfiles.find((p: any) => isAddressEqual(p.address, owner));
   const executorProfile =
     executioner && SecurityCouncilProfiles.find((p: any) => isAddressEqual(p.address, executioner));
 
-  const approvals = relatedProposal?.approvers || gqlProposal?.approvers || [];
-  const vetoes = gqlProposal?.vetoes || relatedProposal?.vetoes || [];
+  const approvals = relatedProposal?.approvers ?? gqlProposal?.approvers ?? [];
+  const vetoes = gqlProposal?.vetoes ?? relatedProposal?.vetoes ?? [];
 
   const hideExecutionTx =
     // if vetoed, no execution
     (vetoes.length && gqlProposal?.executor === null) ||
     // if std and still active, no execution
-    (relatedProposal.isStandard && gqlProposal?.isOptimistic && gqlProposal?.executor === null);
+    (relatedProposal?.isStandard && gqlProposal?.isOptimistic && gqlProposal?.executor === null);
 
+  if (!props.gqlProposal || !props.gqlProposal.creationTxHash || !relatedProposal) {
+    return <Card className="flex flex-col gap-y-4 p-6 shadow-neutral">Loading tx info...</Card>;
+  }
   return (
     <Card className="flex flex-col gap-y-4 p-6 shadow-neutral">
       <Heading size="h4">Transactions</Heading>
@@ -87,7 +86,7 @@ const TransactionsCard: React.FC<ICardResourcesProps> = (props) => {
             <td colSpan={2}>Approvals</td>
           </tr>
           {approvals.map((approver, i) => (
-            <tr>
+            <tr key={`approver-${i}`}>
               <td>
                 <Link
                   target="_blank"
@@ -120,7 +119,7 @@ const TransactionsCard: React.FC<ICardResourcesProps> = (props) => {
           )}
 
           {vetoes.map((vetoer, i) => (
-            <tr>
+            <tr key={`vetoer-${i}`}>
               {" "}
               <td>
                 <Link target="_blank" href={`https://etherscan.io/address/${vetoer.address}`} variant="primary" key={i}>
@@ -172,84 +171,10 @@ const TransactionsCard: React.FC<ICardResourcesProps> = (props) => {
     </Card>
   );
 };
-const CreationCard: React.FC<ICardResourcesProps> = (props) => {
-  if (!props.gqlProposal || !props.gqlProposal.creationTxHash) {
-    return null;
-  }
-  const { gqlProposal } = props;
-  const creationTx = gqlProposal?.creationTxHash;
-
-  return (
-    <Card className="flex flex-col gap-y-4 p-6 shadow-neutral">
-      <Heading size="h4">Creation Tx</Heading>
-      <div className="flex flex-col gap-y-4">
-        <Link
-          target="_blank"
-          href={`https://etherscan.io/tx/${creationTx}`}
-          variant="primary"
-          iconRight={IconType.LINK_EXTERNAL}
-        >
-          View on Etherscan
-        </Link>
-      </div>
-    </Card>
-  );
-};
-
-const ApprovalsCard: React.FC<ICardResourcesProps> = (props) => {
-  const { gqlProposal } = props;
-  if (!gqlProposal || !gqlProposal.approvers.length) {
-    return null;
-  }
-  return (
-    <Card className="flex flex-col gap-y-4 p-6 shadow-neutral">
-      <Heading size="h4">Security Council Member Approvals</Heading>
-      <div className="flex flex-col gap-y-4">
-        {gqlProposal?.approvers.map((approver, i) => (
-          <Link
-            target="_blank"
-            href={`https://etherscan.io/tx/${approver.txHash}`}
-            variant="primary"
-            iconRight={IconType.LINK_EXTERNAL}
-            key={i}
-          >
-            {getSecurityCouncilMemberData(approver.address).name || approver.address}
-          </Link>
-        ))}
-      </div>
-    </Card>
-  );
-};
-
-const VetoCard: React.FC<ICardResourcesProps> = (props) => {
-  const { gqlProposal } = props;
-  if (!gqlProposal || !gqlProposal.vetoes.length) {
-    return null;
-  }
-
-  return (
-    <Card className="flex flex-col gap-y-4 p-6 shadow-neutral">
-      <Heading size="h4">Veto Votes</Heading>
-      <div className="flex flex-col gap-y-4">
-        {gqlProposal?.vetoes.map((approver, i) => (
-          <Link
-            target="_blank"
-            href={`https://etherscan.io/tx/${approver.txHash}`}
-            variant="primary"
-            iconRight={IconType.LINK_EXTERNAL}
-            key={i}
-          >
-            {getSecurityCouncilMemberData(approver.address).name || approver.address}
-          </Link>
-        ))}
-      </div>
-    </Card>
-  );
-};
 
 const DefaultDAOInfoCard: React.FC<ICardResourcesProps> = (props) => {
   const { displayLink = true, title } = props;
-  let { resources, gqlProposal } = props;
+  let { resources } = props;
 
   if (resources == null || resources.length === 0) {
     return <CardEmptyState objectIllustration={{ object: "ARCHIVE" }} heading="No resources were added" />;
@@ -274,32 +199,6 @@ const DefaultDAOInfoCard: React.FC<ICardResourcesProps> = (props) => {
             {resource.name}
           </Link>
         ))}
-      </div>
-    </Card>
-  );
-};
-
-const ExecutionCard: React.FC<ICardResourcesProps> = (props) => {
-  const { gqlProposal } = props;
-
-  if (!gqlProposal || !gqlProposal.executor) {
-    return null;
-  }
-  const executor = gqlProposal?.creator as Address;
-  const executionTx = gqlProposal?.executor?.txHash;
-
-  return (
-    <Card className="flex flex-col gap-y-4 p-6 shadow-neutral">
-      <Heading size="h4">Execution Tx</Heading>
-      <div className="flex flex-col gap-y-4">
-        <Link
-          target="_blank"
-          href={`https://etherscan.io/tx/${executionTx}`}
-          variant="primary"
-          iconRight={IconType.LINK_EXTERNAL}
-        >
-          Etherscan
-        </Link>
       </div>
     </Card>
   );
